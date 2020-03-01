df87e21f14ebdeca771184c694102852bcac9f83
Bruno <brunopr.bpr@gmail.com>
2020-03-01 13:21:50 +0000

    Deleted not used

--- microservice/src/main/java/com/example/ec/explorecali/repo/TourPackageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ec.explorecali.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.ec.explorecali.domain.TourPackage;
-
-public interface TourPackageRepository extends CrudRepository<TourPackage, String>{
-
-}
--- microservice/src/main/java/com/example/ec/explorecali/repo/TourRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ec.explorecali.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.ec.explorecali.domain.Tour;
-
-public interface TourRepository extends CrudRepository<Tour, Integer> {
-
-}
