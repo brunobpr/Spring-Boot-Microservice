0807cc42c32d618cd2bff2ac0d22abb9ba45e7ff
Bruno Ribeiro <brunopr.bpr@gmail.com>
2020-03-01 13:18:38 +0000

    Folders restructured

---
+++ microservice/explorecali/src/main/java/com/example/ec/repo/TourPackageRepository.java
@@ -0,0 +1,18 @@
+package com.example.ec.repo;
+
+import com.example.ec.domain.TourPackage;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Tour Package Repository Interface
+ *
+ * Created by Mary Ellen Bowman
+ */
+public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
+	Optional<TourPackage> findByName(String name);
+	
+}
+
